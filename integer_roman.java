e186c4bfac8988e13bba927a2f1623a174cbb1ce
Drashti Ruparelia <155230292+DecentDrashti@users.noreply.github.com>
2026-02-06 21:35:47 +0530

    Create integer_roman.java

---
+++ integer_roman.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class IntegerToRoman {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] romans = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        String result = "";
+
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                result += romans[i];
+                num -= values[i];
+            }
+        }
+
+        System.out.println(result);
+    }
+}
